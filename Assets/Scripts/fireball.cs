ec180ecb40f69ffcef357230e7be914265bea8ff
Evgeniy <keni.svt@gmail.com>
2023-10-29 21:07:26 +0700

    git add

---
+++ Assets/Scripts/fireball.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireball : MonoBehaviour
+{
+    float speed = 3f;
+    float TimeToDisable = 10f;
+
+    void Start()
+    {
+        StartCoroutine(SetDisabled());
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector2.down * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        StopCoroutine(SetDisabled());
+        gameObject.SetActive(false);
+    }
+
+    IEnumerator SetDisabled()
+    {
+        yield return new WaitForSeconds(TimeToDisable);
+        gameObject.SetActive(false);
+    }
+}
